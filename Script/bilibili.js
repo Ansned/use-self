549d3a9939a9d5bd7f0243808f3c5476cde28da5
Ansned <95335385+Ansned@users.noreply.github.com>
2022-10-28 20:14:52 +0800

    Update bilibili.js

--- Script/bilibili.js
+++ Script/bilibili.js
@@ -21,5 +21,5 @@ hostname = api.biliapi.net, -broadcast.chat.bilibili.com, -*cdn*.biliapi.net, -*
 ***********************************/
 
 var modifiedHeaders = $request.headers;
-modifiedHeaders['Cookie'] = 'buvid3=8890E136-BBAA-096A-6B19-377ECC55F76905746infoc; nostalgia_conf=-1; b_lsid=48F4E91D_1841DEA4F6C; _uuid=67E101095A-BBB3-9AC10-B4A5-FEA4DDFD3551005874infoc; buvid4=38C4BB62-3B0E-D037-7515-83AE4E6BFF2A09081-022102817-GXFOIaD8gdhKsD8MeIV45w==; fingerprint=0a7d147035834d8329dbe1989ccbe7e4; buvid_fp_plain=undefined; SESSDATA=97910f6f,1682501236,27a27*a1; bili_jct=38aa684ae181c04d3ec9ff20f0a694c2; DedeUserID=400825589; DedeUserID__ckMd5=74eca1c0cd603ce4; sid=81xtjn50; PVID=1; buvid_fp=0a7d147035834d8329dbe1989ccbe7e4';
+modifiedHeaders['Cookie'] = 'Buvid=Y2430C29359256014C2CA9BE7E3A7F2C7F29; DedeUserID=400825589; DedeUserID__ckMd5=74eca1c0cd603ce4; SESSDATA=ee58b01c%2C1682511135%2Cd0ff99a2; bili_jct=1e95c4430eab718c65bdb0e561234b09; sid=gpy3j09s';
 $done({headers : modifiedHeaders});
