c69f471e4fda0f3cdf9c2fd42cd90302261abf81
Ansned <95335385+Ansned@users.noreply.github.com>
2022-10-28 19:03:11 +0800

    Update bilibili.js

--- Script/bilibili.js
+++ Script/bilibili.js
@@ -16,7 +16,7 @@
 
 [mitm] 
 
-hostname = hostname = api.biliapi.net, -broadcast.chat.bilibili.com, -*cdn*.biliapi.net, -*tracker*.biliapi.net, *.bili*.*, app.bilibili.com,api.live.bilibili.com,api.vc.bilibili.com, api.bilibili.com, manga.bilibili.com, grpc.biliapi.net
+hostname = api.biliapi.net, -broadcast.chat.bilibili.com, -*cdn*.biliapi.net, -*tracker*.biliapi.net, *.bili*.*, app.bilibili.com,api.live.bilibili.com,api.vc.bilibili.com, api.bilibili.com, manga.bilibili.com, grpc.biliapi.net
 
 ***********************************/
 
