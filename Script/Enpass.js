73bb652a2820490c29be70685d1ce3d97047dc7e
Ansned <95335385+Ansned@users.noreply.github.com>
2022-10-29 16:17:38 +0800

    Update Enpass.js

--- Script/Enpass.js
+++ Script/Enpass.js
@@ -1,11 +1,4 @@
-var obj = JSON.parse($response.body);
-obj.license = "premium";
-obj.info = {
-  purchase_type: "inapp",
-  store: "ios",
-  id: "ENP_IAP_LTP",
-  userid: "000000",
-  transaction_id: "1000000000000000",
-  logo: "",
-};
-$done({ body: JSON.stringify(obj) });
+let obj = JSON.parse($response.body);
+
+obj.license = "premium"
+$done({body: JSON.stringify(obj)});
