b4c9ecf281032fbddc832760af8fddb31eecf098
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-02 11:50:19 +0800

    Update Bilibili_vip_Crack.js

--- Script/Bilibili/Bilibili_vip_Crack.js
+++ Script/Bilibili/Bilibili_vip_Crack.js
@@ -8,7 +8,7 @@
 [rewrite_local]
 
 # > 哔哩哔哩大会员
-^https?:\/\/ap(i|p)\.bilibili\.com\/bilibili\.\w{3,9}\..+\/(View|Play(URL|View|Conf|erOnline)|MainList|ViewProgress)$ url script-request-header BiliBili_VIP_Crack.js
+^https?:\/\/ap(i|p)\.bilibili\.com\/bilibili\.\w{3,9}\..+\/(View|Play(URL|View|Conf|erOnline)|MainList|ViewProgress)$ url script-request-header https://raw.githubusercontent.com/Ansned/use-self/main/Script/Bilibili/Bilibili_vip_Crack.js
 
 [mitm] 
 hostname = *.bilibili.com, 124.239.240.*, 101.89.57.66, 218.94.210.66, 240e:b1:9801:206:11:0:0:*
