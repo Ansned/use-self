65a6ad60e6db692dcabb39579714e6ed69c8449b
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-03 17:35:53 +0800

    Update Bilibili_VIP_Crack.js

--- Rewrite/QuantumultX/Bilibili_VIP_Crack.js
+++ Rewrite/QuantumultX/Bilibili_VIP_Crack.js
@@ -8,7 +8,7 @@
 [rewrite_local]
 
 # > 哔哩哔哩大会员
-^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn|net|org)).*(playconf|player|reply)\.(v3|v2|v1)\.(ViewP|Reply|Play(URL|View|Conf)).*$ url script-request-header https://raw.githubusercontent.com/Ksnsn134/self-use/main/bilibili.js
+^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn|net|org)).*(playconf|player|reply)\.(v3|v2|v1)\.(ViewP|Reply|Play(URL|View|Conf)).*$ url script-request-header https://raw.githubusercontent.com/Ansned/use-self/main/Rewrite/QuantumultX/Bilibili_VIP_Crack.js
 
 [mitm] 
 hostname = api.biliapi.net, -broadcast.chat.bilibili.com, -*cdn*.biliapi.net, -*tracker*.biliapi.net, *.bili*.*, app.bilibili.com,api.live.bilibili.com,api.vc.bilibili.com, api.bilibili.com, manga.bilibili.com, grpc.biliapi.net
