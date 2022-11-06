a8b7d08a864920cb13a728a058071746756c958f
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-06 13:49:28 +0800

    Update Tweetbot.js

--- Script/Tweetbot.js
+++ Script/Tweetbot.js
@@ -6,15 +6,4 @@
 更新时间：2022-11-6
 *******************************
 [rewrite_local]
-https://push.tapbots.com/tweetbot/5/verify_subscription url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Apollo.js
-[mitm] 
-hostname = push.tapbots.com
-*
-*
-*/
-let obj = JSON.parse($response.body);
-
-obj["expires_at"]= 3476097520,
-obj["can_trial"]= true,
-
-$done({body: JSON.stringify(obj)});
+https://push.tapbots.com/tweetbot/5/verify_subscription url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/Tweetbot.js
