1d336d05c6e895e903d7ee0be4478f2522596e35
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-06 23:15:17 +0800

    Update lgzj.js

--- Script/lgzj.js
+++ Script/lgzj.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/footprint-api\.quthing\.com\/vip\/state url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/lgzj.js
+^https:\/\/footprint-api\.quthing\.com\/vip\/state url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/lgzj.js
 
 [mitm] 
 
