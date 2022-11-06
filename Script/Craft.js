2069f009678189257a5ffa145296427dec38010c
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-06 23:10:32 +0800

    Update Craft.js

--- Script/Craft.js
+++ Script/Craft.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api\.craft\.do\/auth\/v2\/profile url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Craft.js
+^https:\/\/api\.craft\.do\/auth\/v2\/profile url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/Craft.js
 
 [mitm] 
 
