8737ee0e18a43aec687e4d263433e63932cfea69
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-16 18:52:45 +0800

    Update and rename Script/hx.js to Script/Crack/hx.js

--- Script/Crack/hx.js
+++ Script/Crack/hx.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api\.shaolinzen\.com\/user\/v1\/info url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/hx.js
+^https:\/\/api\.shaolinzen\.com\/user\/v1\/info url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/Crack/hx.js
 
 [mitm] 
 
