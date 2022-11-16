907876b1f9f7fdbead63c4055aeaaacb9809d938
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-16 18:53:15 +0800

    Update and rename Script/hzk.js to Script/Crack/hzk.js

--- Script/Crack/hzk.js
+++ Script/Crack/hzk.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/www.zhongkao66.com url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/hzk.js
+^http[s]?:\/\/www.zhongkao66.com url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/Crack/hzk.js
 
 [mitm] 
 
