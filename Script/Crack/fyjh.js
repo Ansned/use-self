474541e893c20deaf23bff00e41a7364574cf81f
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-16 18:51:17 +0800

    Update and rename Script/fyjh.js to Script/Crack/fyjh.js

--- Script/Crack/fyjh.js
+++ Script/Crack/fyjh.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/getway-plan\.abuddz\.com\/api\/sync\/putNeedUpdateData url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/fyjh.js
+^https:\/\/getway-plan\.abuddz\.com\/api\/sync\/putNeedUpdateData url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/Crack/fyjh.js
 [mitm] 
 
 hostname = getway-plan.abuddz.com
