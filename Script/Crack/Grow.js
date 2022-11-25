efc6d0812bd56b1400b9947f823f23957b8854d7
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-25 16:06:56 +0800

    Create Grow.js

---
+++ Script/Crack/Grow.js
@@ -0,0 +1,63 @@
+/******************************
+
+脚本功能：Grow - 你的健康+恢复订阅
+下载地址：http://u6v.cn/5IrhMX
+软件版本：1.7.1
+脚本作者：彭于晏💞
+更新时间：2022-9-29
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/api\.revenuecat\.com\/v1\/subscribers.+ url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/Crack/Grow.js
+
+[mitm] 
+hostname = api.revenuecat.com
+
+*******************************/
+
+{
+  "request_date_ms": 1655446431783,
+  "request_date": "2022-06-17T06:13:51Z",
+  "subscriber": {
+    "non_subscriptions": {
+    },
+    "first_seen": "2021-08-18T04:39:18Z",
+    "original_application_version": "304",
+    "other_purchases": {
+    },
+    "management_url": null,
+    "subscriptions": {
+      "grow_299_1m": {
+        "is_sandbox": false,
+        "ownership_type": "PURCHASED",
+        "billing_issues_detected_at": null,
+        "period_type": "active",
+        "expires_date": "2099-08-21T04:41:11Z",
+        "grace_period_expires_date": null,
+        "unsubscribe_detected_at": "2021-08-18T20:32:00Z",
+        "original_purchase_date": "2021-08-18T04:41:12Z",
+        "purchase_date": "2021-08-18T04:41:11Z",
+        "store": "app_store"
+      }
+    },
+    "entitlements": {
+      "grow.pro": {
+        "grace_period_expires_date": null,
+        "purchase_date": "2021-08-18T04:41:11Z",
+        "product_identifier": "grow_299_1m",
+        "expires_date": "2099-08-21T04:41:11Z"
+      }
+    },
+    "original_purchase_date": "2021-08-18T04:38:27Z",
+    "original_app_user_id": "$RCAnonymousID:7fe786499ddd4e3688a26f5a1cb3f367",
+    "last_seen": "2022-06-17T06:13:51Z"
+  }
+}
