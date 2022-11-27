86800e9876aa4bdb27eb1701b5acabe20b0e6b71
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-28 01:22:58 +0800

    Update happyday.js

--- Script/Crack/happyday.js
+++ Script/Crack/happyday.js
@@ -3,7 +3,7 @@ var obj = JSON.parse(body);
 
 obj.subscriber.entitlements = {
 "pro":{
-              "expires_date":"2029-05-26T05:05:04Z",
+              "expires_date":"2099-05-26T05:05:04Z",
               "product_identifier":"happy_449_1y",
               "purchase_date":"2022-11-25T08:16:50Z"
     }
@@ -11,7 +11,7 @@ obj.subscriber.entitlements = {
 obj.subscriber.subscriptions ={
       "happy_449_1y":{
               "billing_issues_detected_at":null,
-              "expires_date":"2029-05-26T05:05:04Z",
+              "expires_date":"2099-05-26T05:05:04Z",
               "is_sandbox":false,
               "original_purchase_date":"2022-11-25T08:16:51Z",
               "period_type":"normal",
