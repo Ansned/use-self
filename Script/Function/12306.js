bb81fa66683e30dd90df3f6161f2b0cd1838df39
Ansned <95335385+Ansned@users.noreply.github.com>
2022-11-27 23:37:21 +0800

    Create 12306.js

--- Script/Function/12306.js
+++ Script/Function/12306.js
@@ -5,7 +5,7 @@
 请检查你的分流是否有ad.12306.cn，如果有，需要去除（可使用解析器排出#out=12306），不然重写是无法生效的，同时检查是否有与其冲突的重写
 
 [rewrite_local]
-^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url script-response-body https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Script/12306.js
+^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url script-response-body https://raw.githubusercontent.com/Ansned/use-self/main/Script/Function/12306.js
 
 [mitm]
 hostname = ad.12306.cn
