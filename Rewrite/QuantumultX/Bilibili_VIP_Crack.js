1599b12e3ed5fb5922b97942b2a83e646671f06b
Ansned <95335385+Ansned@users.noreply.github.com>
2022-12-03 13:53:38 +0800

    Update Bilibili_VIP_Crack.js

--- Rewrite/QuantumultX/Bilibili_VIP_Crack.js
+++ Rewrite/QuantumultX/Bilibili_VIP_Crack.js
@@ -45,7 +45,7 @@ var headers = $request['headers'];
 var modifiedheaders = {"Cookie":"DedeUserID=54343099; DedeUserID__ckMd5=492068d79771a5f4; SESSDATA=bf67508b%2C1685590493%2C63442ec2; bili_jct=440fdb025ca0b5c1474bad93be1660ac; sid=n4iy5u82","Authorization":"identify_v1 4b188da45eb374194c5d7398d046a3c2","User-Agent":"bili-universal/70700200 os/ios model/iPhone X mobi_app/iphone osVer/15.4.1 network/2","x-bili-locale-bin":"Eg4KAnpoEgRIYW5zGgJDTg==","x-bili-device-bin":"CAEQqJnbIRokWkY0QUQyRjdFQTNEN0Q0MjRCMTBCNjFCRjhBQjg4MDJGN0VGIgZpcGhvbmUqA2lvczIFcGhvbmU6BWFwcGxlQgVBcHBsZUoIaVBob25lIFhSBjE1LjQuMWoFNy43LjByQDlFQjgzOTk0NzY2RUE5ODVERjRGNEM2QkEzNDc3QzBGMjAyMTA3MDQxMzQzNTNCMjlENzg5N0UxRUI2QjJDNDV4vfXU98sw","x-bili-metadata-bin":"CiA0YjE4OGRhNDVlYjM3NDE5NGM1ZDczOThkMDQ2YTNjMhIGaXBob25lGgVwaG9uZSComdshKgVhcHBsZTIkWkY0QUQyRjdFQTNEN0Q0MjRCMTBCNjFCRjhBQjg4MDJGN0VGOgNpb3M=","x-bili-fawkes-req-bin":"CgZpcGhvbmUSBHByb2QaEGZmZmZmZmZmYzUxYTQ1OWU="};
 
 var uid = Number(cookie2object(headers.Cookie).DedeUserID);
-let uids = [400825589,3493083748502332];
+let uids = [400825589, 3493083748502332];
 let result = uids.includes(uid);
 if (result) {
 headers['Cookie'] = modifiedheaders.Cookie;
@@ -55,5 +55,5 @@ headers['x-bili-locale-bin'] = modifiedheaders['x-bili-locale-bin'];
 headers['x-bili-device-bin'] = modifiedheaders['x-bili-device-bin'];
 headers['x-bili-metadata-bin'] = modifiedheaders['x-bili-metadata-bin'];
 headers['x-bili-fawkes-req-bin'] = modifiedheaders['x-bili-fawkes-req-bin'];
-
+｝
 $done({ 'headers': headers });
