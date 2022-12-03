0c0d7a790dc293d140d274a7d023a12f07670158
Ansned <95335385+Ansned@users.noreply.github.com>
2022-12-03 13:46:12 +0800

    Update Bilibili_VIP_Crack.js

--- Rewrite/QuantumultX/Bilibili_VIP_Crack.js
+++ Rewrite/QuantumultX/Bilibili_VIP_Crack.js
@@ -31,8 +31,23 @@ hostname = api.biliapi.net, -broadcast.chat.bilibili.com, -*cdn*.biliapi.net, -*
 hostname = api.biliapi.net, -broadcast.chat.bilibili.com, -*cdn*.biliapi.net, -*tracker*.biliapi.net, *.bili*.*, app.bilibili.com,api.live.bilibili.com,api.vc.bilibili.com, api.bilibili.com, manga.bilibili.com, grpc.biliapi.net
 */
 
+const cookie2object = (cookie) => {
+  var obj = {};
+  var arr = cookie.split("; ");
+  arr.forEach(function (val) {
+    var brr = val.split("=");
+    obj[brr[0]] = brr[1];
+  });
+  return obj;
+};
+
 var headers = $request['headers'];
-modifiedheaders = {"Cookie":"DedeUserID=54343099; DedeUserID__ckMd5=492068d79771a5f4; SESSDATA=bf67508b%2C1685590493%2C63442ec2; bili_jct=440fdb025ca0b5c1474bad93be1660ac; sid=n4iy5u82","Authorization":"identify_v1 4b188da45eb374194c5d7398d046a3c2","User-Agent":"bili-universal/70700200 os/ios model/iPhone X mobi_app/iphone osVer/15.4.1 network/2","x-bili-locale-bin":"Eg4KAnpoEgRIYW5zGgJDTg==","x-bili-device-bin":"CAEQqJnbIRokWkY0QUQyRjdFQTNEN0Q0MjRCMTBCNjFCRjhBQjg4MDJGN0VGIgZpcGhvbmUqA2lvczIFcGhvbmU6BWFwcGxlQgVBcHBsZUoIaVBob25lIFhSBjE1LjQuMWoFNy43LjByQDlFQjgzOTk0NzY2RUE5ODVERjRGNEM2QkEzNDc3QzBGMjAyMTA3MDQxMzQzNTNCMjlENzg5N0UxRUI2QjJDNDV4vfXU98sw","x-bili-metadata-bin":"CiA0YjE4OGRhNDVlYjM3NDE5NGM1ZDczOThkMDQ2YTNjMhIGaXBob25lGgVwaG9uZSComdshKgVhcHBsZTIkWkY0QUQyRjdFQTNEN0Q0MjRCMTBCNjFCRjhBQjg4MDJGN0VGOgNpb3M=","x-bili-fawkes-req-bin":"CgZpcGhvbmUSBHByb2QaEGZmZmZmZmZmYzUxYTQ1OWU="};
+var modifiedheaders = {"Cookie":"DedeUserID=54343099; DedeUserID__ckMd5=492068d79771a5f4; SESSDATA=bf67508b%2C1685590493%2C63442ec2; bili_jct=440fdb025ca0b5c1474bad93be1660ac; sid=n4iy5u82","Authorization":"identify_v1 4b188da45eb374194c5d7398d046a3c2","User-Agent":"bili-universal/70700200 os/ios model/iPhone X mobi_app/iphone osVer/15.4.1 network/2","x-bili-locale-bin":"Eg4KAnpoEgRIYW5zGgJDTg==","x-bili-device-bin":"CAEQqJnbIRokWkY0QUQyRjdFQTNEN0Q0MjRCMTBCNjFCRjhBQjg4MDJGN0VGIgZpcGhvbmUqA2lvczIFcGhvbmU6BWFwcGxlQgVBcHBsZUoIaVBob25lIFhSBjE1LjQuMWoFNy43LjByQDlFQjgzOTk0NzY2RUE5ODVERjRGNEM2QkEzNDc3QzBGMjAyMTA3MDQxMzQzNTNCMjlENzg5N0UxRUI2QjJDNDV4vfXU98sw","x-bili-metadata-bin":"CiA0YjE4OGRhNDVlYjM3NDE5NGM1ZDczOThkMDQ2YTNjMhIGaXBob25lGgVwaG9uZSComdshKgVhcHBsZTIkWkY0QUQyRjdFQTNEN0Q0MjRCMTBCNjFCRjhBQjg4MDJGN0VGOgNpb3M=","x-bili-fawkes-req-bin":"CgZpcGhvbmUSBHByb2QaEGZmZmZmZmZmYzUxYTQ1OWU="};
+
+var uid = Number(cookie2object(headers.Cookie).DedeUserID);
+let uids = [400825589,3493083748502332];
+let result = uids.includes(uid);
+if (result) {
 headers['Cookie'] = modifiedheaders.Cookie;
 headers['Authorization'] =  modifiedheaders.Authorization;
 headers['User-Agent'] = modifiedheaders['User-Agent'];
