a335e26fbaf13b34589805b07017c3a595b1c958
Ansned <95335385+Ansned@users.noreply.github.com>
2022-12-26 19:17:10 +0800

    Update bli.js

--- Script/Bilibili/bli.js
+++ Script/Bilibili/bli.js
@@ -1,16 +1,17 @@
 /*
 脚本功能：哔哩哔哩解锁大会员
-软件版本：6.90.0
-更新时间：2022-10-31
+软件版本：7.7.0
+更新时间：2022-12-26
 使用声明：⚠️此脚本仅供学习与交流，
-        请勿转载与贩卖！⚠️⚠️⚠️
-*******************************
+请勿转载与贩卖！⚠️⚠️⚠️
+---
 [rewrite_local]
-# > 哔哩哔哩大会员
-^https?:\/\/ap(i|p)\.bilibili\.com\/bilibili\.\w{3,9}\..+\/(View|Play(URL|View|Conf|erOnline)|MainList|ViewProgress)$ url script-request-header https://Ansned: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@raw.githubusercontent.com/Ansned/use-self/main/Script/Bilibili/bli.js
-[mitm] 
-hostname = *.bilibili.com, 124.239.240.*, 101.89.57.66, 218.94.210.66, 240e:b1:9801:206:11:0:0:*
+> 哔哩哔哩大会员
+^http[s]?://((app|api).(\w{2,15})?.(com|cn|net|org)).(playconf|player|reply).(v3|v2|v1).(ViewP|Reply|Play(URL|View|Conf)).$ url script-request-header https://Ansned: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@raw.githubusercontent.com/Ansned/use-self/main/Script/Bilibili/bli.js
+[mitm]
+hostname = api.biliapi.net, -broadcast.chat.bilibili.com, -cdn.biliapi.net, -tracker.biliapi.net, .bili.*, app.bilibili.com,api.live.bilibili.com,api.vc.bilibili.com, api.bilibili.com, manga.bilibili.com, grpc.biliapi.net
 */
+
 
 const cookie2object = (cookie) => {
   var obj = {};
