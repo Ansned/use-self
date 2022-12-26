0ad175d6770fb49216e78cf26f216c999efe4af4
Ansned <95335385+Ansned@users.noreply.github.com>
2022-12-26 19:26:04 +0800

    Update Bilibili_VIP_Crack.js

--- Rewrite/QuantumultX/Bilibili_VIP_Crack.js
+++ Rewrite/QuantumultX/Bilibili_VIP_Crack.js
@@ -9,7 +9,7 @@
 [rewrite_local]
 
 # > 哔哩哔哩大会员
-^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn|net|org)).*(playconf|player|reply)\.(v3|v2|v1)\.(ViewP|Reply|Play(URL|View|Conf)).*$ url script-request-header https://raw.githubusercontent.com/Ansned/use-self/main/Rewrite/QuantumultX/Bilibili_VIP_Crack.js
+^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn|net|org)).*(playconf|player|reply)\.(v3|v2|v1)\.(ViewP|Reply|Play(URL|View|Conf)).*$ url script-request-header https://Ansned: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@raw.githubusercontent.com/Ansned/use-self/main/Rewrite/QuantumultX/Bilibili_VIP_Crack.js
 
 [mitm] 
 
